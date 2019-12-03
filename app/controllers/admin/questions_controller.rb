5542b1d0bd951da955748b5569465e08479b841f
ShimizuSho <YASU912495@gmail.com>
2019-12-03 16:02:42 +0000

    contactsにemailカラム追加

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,13 +1,13 @@
 class Admin::QuestionsController < ApplicationController
 
 	def index
-		@questions = Question.page(params[:page]).per(30).reverse_order
+		@questions = Question.page(params[:page]).per(50).reverse_order
 	end
 
 	def show
 		@question = Question.find(params[:id])
 		@comment = Comment.new
-		@comments = Comment.all
+		@comments = @question.comments
 	end
 
 	def destroy
