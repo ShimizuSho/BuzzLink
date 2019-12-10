ccab36a00eebc8963a893a5e0f18605cb5806545
ShimizuSho <YASU912495@gmail.com>
2019-12-10 04:35:01 +0000

    退会処理の修正

--- app/models/folder.rb
+++ app/models/folder.rb
@@ -1,6 +1,6 @@
 class Folder < ApplicationRecord
 
 	belongs_to :user
-	has_many :posts, dependent: :destroy
+	# has_many :posts, dependent: :destroy
 	has_many :folder_contents, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
   attachment :profile_image
 
   has_many :followers, dependent: :destroy
-  has_many :posts
-  has_many :favorites
+  has_many :posts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :question_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
